03b0053de5ffde6715d549605cd5e8b599811da1
Ansh2 <ansh.nynj@gmail.com>
2024-01-20 15:17:18 -0500

    added shopping list file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-import './styles/App.css';
-import './styles/styles.css';
-import './bootstrap/dist/css/bootstrap.min.css';
+import ShoppingList from "./ShoppingList.js";
+import "./styles/App.css";
+import "./styles/styles.css";
+import "./bootstrap/dist/css/bootstrap.min.css";
 import { BsChevronCompactDown } from "react-icons/bs";
 import { LuClipboardList } from "react-icons/lu";
 import { LuClipboardCheck } from "react-icons/lu";
@@ -14,29 +15,35 @@ function App() {
   return (
     <div className="App">
       <div className="container-fluid">
-        <div className='row header'>
+        <div className="row header">
           <div className="col-md-6 col-xs-12">
-            <h1 id='title'>BHAI</h1>
-            <h3 id='subtitle'>(Budget Helper AI)</h3>
+            <h1 id="title">BHAI</h1>
+            <h3 id="subtitle">(Budget Helper AI)</h3>
           </div>
           <div className="col-md-6 col-xs-12">
             <h3 className="sub">Your Personal Finance Bro</h3>
-            <LuClipboardList className="icon1"/>
-            <FaArrowRightLong className="icon2"/>
-            <LuClipboardCheck className="icon3" /> 
+            <LuClipboardList className="icon1" />
+            <FaArrowRightLong className="icon2" />
+            <LuClipboardCheck className="icon3" />
           </div>
         </div>
-        <div className='row transition'>
-          <a href='#' onClick={scroll}><div className='col-12'><BsChevronCompactDown /></div></a>
+        <div className="row transition">
+          <a href="#" onClick={scroll}>
+            <div className="col-12">
+              <BsChevronCompactDown />
+            </div>
+          </a>
         </div>
       </div>
       <div className="container-fluid">
-        <div className="row" id='main'>
-          <div className="col-md-6 col-xs-12" id='left'>
-          </div>
-          <div className="col-md-6 col-xs-12" id='right'>
-          </div>
+        <div className="row" id="main">
+          <div className="col-md-6 col-xs-12" id="left"></div>
+          <div className="col-md-6 col-xs-12" id="right"></div>
         </div>
+      </div>
+
+      <div>
+        <ShoppingList />
       </div>
     </div>
   );
